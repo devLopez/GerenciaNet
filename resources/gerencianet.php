4bee3b731222ffa677fe3ffe07761066b8fbd3d7
Matheus Lopes Santos <informatica.fadenor@unimontes.br>
2017-12-01 10:15:22 -0200

    Criação de service provider para compatibilidade com laravel Inclusão do package auto-discovery para laravel 5.5 Inclusão do arquivo de configuração do package

---
+++ resources/gerencianet.php
@@ -0,0 +1,7 @@
+<?php
+
+    return [
+        'client_id'     => env('GERENCIANET_CLIENT_ID'),
+        'client_secret' => env('GERENCIANET_CLIENT_SECRET'),
+        'sandbox'       => env('GERENCIANET_SANDBOX')
+    ];
---
+++ src/ServiceProviders/GerencianetServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Igrejanet\GerenciaNet\ServiceProviders;
+
+use Gerencianet\Gerencianet;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * GerencianetServiceProvider
+ *
+ * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @since   01/12/2017
+ * @package Igrejanet\GerenciaNet\ServiceProviders
+ */
+class GerencianetServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+    
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../resources/gerencianet.php',
+            'gerencianet'
+        );
+
+        $this->app->singleton(Gerencianet::class, function () {
+            $options = config('gerencianet');
+
+            return new Gerencianet($options);
+        });
+    }
+}
